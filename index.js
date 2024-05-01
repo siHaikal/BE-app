3c13f5b045c6bac231f83085855c7c3329e9d3ef
siHaikal <haikalcc39@gmail.com>
2024-05-01 13:36:12 +0700

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 3200
 
 const app = express()
 const sequalize = require('./dbConfig')
+const loginEndPoint = require('./Route/login')
 
 sequalize.sync().then(() => {
     console.log('data ready')
@@ -11,10 +12,12 @@ sequalize.sync().then(() => {
 app.use(cors())
 app.use(express.json());
 
+app.get('/', ((req, res) => res.send ('hello world')))
+app.post('/login', loginEndPoint)
 
 
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
 
